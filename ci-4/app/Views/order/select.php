9dcbf7272746b9eba1aed774ffadeeaf98c7c8ca
DarumaDaichi <sudarmoadi89@gmail.com>
2020-10-14 09:12:45 +0700

    Video 35
    Membuat view order dan menampilkan data dalam database vorder

---
+++ ci-4/app/Views/order/select.php
@@ -0,0 +1,50 @@
+<?= $this->extend('template/admin') ?>
+
+<?= $this->section('content') ?>
+
+<div class="row">
+    <div class="col">
+        <?= $judul?>
+    </div>
+</div>
+
+<div class="row mt-2">
+    <div class="col">
+        <table class="table">
+            <tr>
+                <th>No</th>
+                <th>Id Order</th>
+                <th>Pelanggan</th>
+                <th>Tanggal</th>
+                <th>Total</th>
+                <th>Bayar</th>
+                <th>Kembali</th>
+                <th>Status</th>
+            </tr>
+
+            <?php $no = 1 ?>
+            <?php foreach ($order as $value) : ?>
+                <tr>
+                    <td><?= $no++ ?></td>
+                    <td><?= $value['idorder'] ?></td>
+                    <td><?= $value['pelanggan'] ?></td>
+                    <td><?= $value['tglorder'] ?></td>
+                    <td><?= $value['total'] ?></td>
+                    <td><?= $value['bayar'] ?></td>
+                    <td><?= $value['kembali'] ?></td>
+                    <?php 
+                        if($value['status'] == 1)
+                        {
+                            $status = '<div class = "btn btn-primary">LUNAS</div>';
+                        }else{
+                            $status = '<div class = "btn btn-danger">BAYAR</div>';
+                        }
+                    ?>
+                    <td><?= $status ?></td>
+                </tr>
+            <?php endforeach;?>
+        </table>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
